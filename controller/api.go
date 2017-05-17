d40fcda044468f175a389d6dd5cbb24906c3a2f3
Soam Vasani <soamvasani@platform9.com>
2017-05-17 10:22:04 -0700

    revert api.go

--- controller/api.go
+++ controller/api.go
@@ -96,7 +96,7 @@ func (api *API) getLogDBConfig(dbType string) logDBConfig {
 
 func (api *API) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	fmt.Fprintf(w, "{\"message\": \"Fission API\", \"version\": \"0.2.0\"}\n")
+	fmt.Fprintf(w, "{\"message\": \"Fission API\", \"version\": \"0.1.0\"}\n")
 }
 
 func (api *API) Serve(port int) {
