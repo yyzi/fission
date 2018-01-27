8426c83de3b0858eec748dd77dfa8050ad6d3e8a
Soam Vasani <soamvasani@platform9.com>
2018-01-27 11:29:10 -0800

    Use POST in test_connect

--- test/tests/test_connect.sh
+++ test/tests/test_connect.sh
@@ -8,6 +8,6 @@ go get github.com/rakyll/hey
 for i in 1 2 3 4 5
 do
     # 20 requests, one at a time.  Generates a latency and error rate report.
-    go run $GOPATH/src/github.com/rakyll/hey/hey.go -n 20 -c 1 $FISSION_URL
+    go run $GOPATH/src/github.com/rakyll/hey/hey.go -m POST -n 20 -c 1 $FISSION_URL
     sleep 5
 done
