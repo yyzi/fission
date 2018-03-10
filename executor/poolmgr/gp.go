20cdb29e1e9b38a9b977be85b2277e55b8e08962
smruthi2187 <smruthi@platform9.com>
2018-03-09 19:09:53 -0800

    Does not work when tested. saving the work.

--- executor/poolmgr/gp.go
+++ executor/poolmgr/gp.go
@@ -469,10 +469,13 @@ func (gp *GenericPool) createPool() error {
 
 	// Use long terminationGracePeriodSeconds for connection draining in case that
 	// pod still runs user functions.
-	gracePeriodSeconds := int64(6 * 60)
+
+	//gracePeriodSeconds := int64(6 * 60)
+	gracePeriodSeconds := int64(0)
 	if gp.env.Spec.TerminationGracePeriod > 0 {
 		gracePeriodSeconds = gp.env.Spec.TerminationGracePeriod
 	}
+
 
 	podAnnotation := make(map[string]string)
 
